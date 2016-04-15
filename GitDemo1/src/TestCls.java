7405d0cba3d7d13ab7a6d5952a2e514378ba2ae7
Smaity90 <amaremail2.smaity@gmail.com>
2016-04-15 22:05:17 +0530

    Update TestCls.java

--- GitDemo1/src/TestCls.java
+++ GitDemo1/src/TestCls.java
@@ -4,6 +4,7 @@ public class TestCls {
 	public static void main(String[] args) {
 		System.out.println("hello git");
 		System.out.println("hello git again");
+		System.out.println("hello git frm github");
 	}
 
 }
